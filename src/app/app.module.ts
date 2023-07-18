b3515d7668d90b53f1006e07d9fae2364b4195c3
Esmeralda <e.mendoza@dappertechnologies.com>
2023-07-18 17:43:05 -0500

    application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BannerComponent } from './components/nav-bar/banner/banner.component';
+import { FooterComponent } from './components/nav-bar/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BannerComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
